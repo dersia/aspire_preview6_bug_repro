075c5b69b896a245d6a6b7394cefe132db3ab977
Sia Ghassemi <sia@sia-consulting.eu>
2024-04-25 09:14:29 +0200

    added repro of bug

---
+++ AspireApp1.AppHost/Program.cs
@@ -0,0 +1,11 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.AspireApp1_ApiService>("apiservice");
+
+builder.AddProject<Projects.AspireApp1_Web>("webfrontend")
+    .WithReference(cache)
+    .WithReference(apiService);
+
+builder.Build().Run();
